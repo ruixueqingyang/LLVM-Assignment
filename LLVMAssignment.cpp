8e46c279bfe0a08c4f1a03a648b5c98e319d8a58
NeilBian <bianstamina@gmail.com>
2018-11-28 21:12:46 +0800

    direct,indirect (PHI Argument)

--- LLVMAssignment.cpp
+++ LLVMAssignment.cpp
@@ -33,6 +33,19 @@
 #include <llvm/Bitcode/ReaderWriter.h>
 #endif
 using namespace llvm;
+
+/*********add**********/
+#include <iostream>
+#include <list>
+#include <llvm/IR/BasicBlock.h>   //  BasicBlock
+#include <llvm/IR/Function.h>     //  Function
+#include <llvm/IR/Instruction.h>  //  Instruction
+#include <llvm/IR/Instructions.h> //  CallInst  PHINode
+#include <llvm/IR/Module.h>       //  Moudle
+#include <llvm/IR/User.h>         //  User
+using namespace std;
+/*********add**********/
+
 #if LLVM_VERSION_MAJOR >= 4
 static ManagedStatic<LLVMContext> GlobalContext;
 static LLVMContext &getGlobalContext() { return *GlobalContext; }
@@ -60,14 +73,160 @@ char EnableFunctionOptPass::ID = 0;
 /// Updated 11/10/2017 by fargo: make all functions
 /// processed by mem2reg before this pass.
 struct FuncPtrPass : public ModulePass {
-  static char ID;  // Pass identification, replacement for typeid
+  static char ID; // Pass identification, replacement for typeid
   FuncPtrPass() : ModulePass(ID) {}
 
+  list<StringRef> funcList;
+  list<Value *> valueList;
+
+  void getPHINode(PHINode *pHINode) {
+    // incoming_values()的返回值类型是op_range，user类型
+    for (Value *inComingVal : pHINode->incoming_values()) {
+
+      if (pHINode = dyn_cast<PHINode>(inComingVal)) {
+        errs() << "PHINode / getPHINode"
+               << "\n";
+        getPHINode(pHINode);
+      } else if (Argument *argument = dyn_cast<Argument>(inComingVal)) {
+        errs() << "Argument / getPHINode"
+               << "\n";
+      } else if (Function *func = dyn_cast<Function>(inComingVal)) {
+        errs() << "Function / getPHINode"
+               << "\n";
+        funcList.push_back(func->getName());
+        errs() << func->getName() << "\n";
+      }
+    }
+  }
+  /*
+   * User: 所有llvm节点的基类,
+   *对User类的操作都直接作用于其所指向的llvm的Value类型
+   *
+   **/
+  void getArgument(Argument *arg) {
+    // Return the index of this formal argument
+    int index = arg->getArgNo();
+    errs() << "index " << index << "\n";
+    Function *parentFunc = arg->getParent();
+    // iterator_range< use_iterator >   uses (),
+    for (User *user : parentFunc->users()) {
+      if (CallInst *callInst = dyn_cast<CallInst>(user)) {
+        // 获得参数操作数
+        Value *value = callInst->getArgOperand(index);
+        if (Function *func = dyn_cast<Function>(value)) {
+          errs() << "Function / CallInst / getArgument"
+                 << "\n";
+          funcList.push_back(func->getName());
+        } else if (PHINode *pHINode = dyn_cast<PHINode>(value)) {
+          errs() << "PHINode / CallInst / getArgument"
+                 << "\n";
+          getPHINode(pHINode);
+        } else if (LoadInst *loadInst = dyn_cast<LoadInst>(value)) {
+          errs() << "LoadInst / CallInst / getArgument"
+                 << "\n";
+        } else if (Argument *argument = dyn_cast<Argument>(value)) {
+          errs() << "Argument / CallInst / getArgument"
+                 << "\n";
+          getArgument(argument);
+        }
+      }
+      // else if (PHINode *pHINode = dyn_cast<PHINode>(user)) {
+      //   for (User *user : pHINode->users()) {
+      //     if (CallInst *callInst = dyn_cast<CallInst>(user)) {
+      //       Value *value = callInst->getOperand(index);
+      //       if (Function *func = dyn_cast<Function>(value)) {
+      //         errs() << "Function / PHINode / getArgument"
+      //                << "\n";
+      //         funcList.push_back(func->getName());
+      //         errs() << func->getName() << "\n";
+      //       }
+      //     }
+      //   }
+      // }
+    }
+  }
+
+  // show the result
+  void showResult(unsigned line) {
+    errs() << line << " : ";
+    auto it = funcList.begin();
+    auto ie = funcList.end();
+    if (it != ie) {
+      errs() << *it;
+      ++it;
+    }
+    for (; it != ie; ++it) {
+      errs() << ", " << *it;
+    }
+    errs() << "\n";
+  }
+  /** Value: the base class of all values
+   * Argument: an incoming formal argument to a Function
+   * PHINode:
+   * CallInst: function call
+   */
+  void getFunction(CallInst *callInst) {
+    // Value　is the base class of all values
+    Value *value = callInst->getCalledValue();
+    if (LoadInst *loadInst = dyn_cast<LoadInst>(value)) {
+      errs() << "LoadInst / getFunction"
+             << "\n";
+    } else if (PHINode *pHINode = dyn_cast<PHINode>(value)) {
+      errs() << "PHINode / getFunction"
+             << "\n";
+      getPHINode(pHINode);
+    } else if (Argument *argument = dyn_cast<Argument>(value)) {
+      errs() << "Argument / getFunction"
+             << "\n";
+      getArgument(argument);
+    } else if (CallInst *callInst = dyn_cast<CallInst>(value)) {
+      errs() << "CallInst / getFunction"
+             << "\n";
+    }
+  }
+
   bool runOnModule(Module &M) override {
-    errs() << "Hello: ";
-    errs().write_escaped(M.getName()) << '\n';
-    M.dump();
-    errs() << "------------------------------\n";
+    // errs() << "Hello: ";
+    // errs().write_escaped(M.getName()) << '\n';
+    // M.dump();
+    // errs() << "------------------------------\n";
+    // Moudle迭代
+    for (Module::iterator md_i = M.begin(), md_e = M.end(); md_i != md_e;
+         ++md_i) {
+      // Function迭代
+      for (Function::iterator func_i = md_i->begin(), func_e = md_i->end();
+           func_i != func_e; ++func_i) {
+        // BasicBlock迭代
+        for (BasicBlock::iterator bb_i = func_i->begin(), bb_e = func_i->end();
+             bb_i != bb_e; ++bb_i) {
+          // 指令类型
+          Instruction *inst = dyn_cast<Instruction>(bb_i);
+          // Isa函数的作用是测试给定的变量是否具有指定的类型
+          if (isa<CallInst>(bb_i)) {
+            CallInst *callInst = dyn_cast<CallInst>(inst);
+            // 返回调用的函数，如果这是间接函数调用，则返回null
+            Function *func = callInst->getCalledFunction();
+            // 调用函数的行号
+            unsigned callLine = callInst->getDebugLoc().getLine();
+            // 如果是间接引用
+            if (func == NULL) {
+              errs() << "************indirect function invocation************"
+                     << "\n";
+              getFunction(callInst);
+              showResult(callLine);
+            } else {
+              // Returns true if the function's name starts with "llvm."
+              if (!func->isIntrinsic()) {
+                errs() << "************direct function invocation************"
+                       << "\n";
+                errs() << callLine << " : " << func->getName() << "\n";
+              }
+            }
+          }
+        }
+      }
+    }
+
     return false;
   }
 };
@@ -76,9 +235,8 @@ char FuncPtrPass::ID = 0;
 static RegisterPass<FuncPtrPass> X("funcptrpass",
                                    "Print function call instruction");
 
-static cl::opt<std::string> InputFilename(cl::Positional,
-                                          cl::desc("<filename>.bc"),
-                                          cl::init(""));
+static cl::opt<std::string>
+    InputFilename(cl::Positional, cl::desc("<filename>.bc"), cl::init(""));
 
 int main(int argc, char **argv) {
   LLVMContext &Context = getGlobalContext();
